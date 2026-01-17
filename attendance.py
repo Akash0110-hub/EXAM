9244ca9443f09be58fa30d1dfd06759272ae60d0
Akash0110-hub <akashgshettar10@gmail.com>
2026-01-17 11:23:18 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,8 +1,8 @@
-def eligible(held, attended):
-    return (attended / held) * 100 >= 75
+def check_attendance(classes_held, classes_attended):
+    percentage = (classes_attended / classes_held) * 100
+    return "Eligible" if percentage >= 75 else "Not Eligible"
+
 
 if __name__ == "__main__":
-    h = int(input())
-    a = int(input())
-    print("Eligible" if eligible(h,a) else "Not Eligible")
-
+    # Optional default values for manual run
+    print(check_attendance(100, 80))
