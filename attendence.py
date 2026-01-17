bf4e1b8285300da113a70cc51196797b941acc62
Akash0110-hub <akashgshettar10@gmail.com>
2026-01-17 11:18:56 +0530

    Delete attendence.py

--- attendence.py
+++
@@ -1,8 +0,0 @@
-def eligible(held, attended):
-    return (attended / held) * 100 >= 75
-
-if __name__ == "__main__":
-    h = int(input())
-    a = int(input())
-    print("Eligible" if eligible(h,a) else "Not Eligible")
-
